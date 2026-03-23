2348419833314a490c86f2f43b269202d70c84fd
Kevin Videau <kevin.videau@gmail.com>
2026-03-23 18:20:46 +0100

    chore: remove unused files and clean up code

--- src/app/shared/ui/category-badge/category-badge.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-category-badge',
-  imports: [],
-  templateUrl: './category-badge.html',
-  styleUrl: './category-badge.scss',
-})
-export class CategoryBadge {}
--- src/app/shared/ui/header-tab/header-tab.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-header-tab',
-  imports: [],
-  templateUrl: './header-tab.html',
-  styleUrl: './header-tab.scss',
-})
-export class HeaderTab {}
